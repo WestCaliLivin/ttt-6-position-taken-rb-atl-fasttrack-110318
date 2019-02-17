c9d011be1f069fa12174fd9c66721f38acb96605
Dylan Harris <dylanharris23@gmail.com>
2019-02-17 17:55:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@
 # it could be 0, 4, 7, 1
 # code your #position_taken? method here!
 def position_taken?(board, index)
-   if position == "" || position == " " || position == nil
+   if position[index] == "" || position[index] == " " || position[index] == nil
      return false
  else
      return true
