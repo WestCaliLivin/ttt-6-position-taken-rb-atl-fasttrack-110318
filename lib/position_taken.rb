1a1ddb13093262cdcc9559f5a24cfbcd40bc18fd
Dylan Harris <dylanharris23@gmail.com>
2019-02-17 17:46:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,11 @@
+#board is an arry of strings 
+# ["x", ""," ", "",]
+# index is a number to check 
+# it could be 0, 4, 7, 1
 # code your #position_taken? method here!
 def position_taken?(board, index)
-   if board[index] == ""
+  puts board.inspect
+   puts index
+   puts board[index]
    
 end
